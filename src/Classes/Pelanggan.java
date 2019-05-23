84aa804984be693c9788fe5625d259ad3098e11f
niken <niken@niken-PC>
2019-05-23 12:31:04 +0700

    TugasPBO245

---
+++ src/Classes/Pelanggan.java
@@ -0,0 +1,35 @@
+package Classes;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author niken
+ */
+public class Pelanggan {
+    protected String nopel;
+    protected String nama;
+    
+    public void setNOPEL(String nopel)
+    {
+        this.nopel = nopel;
+    }
+    public void setNama(String nama)
+    {
+        this.nama = nama;
+    }
+    public String getNOPEL()
+    {
+        return this.nopel;
+    }
+    public String getNama()
+    {
+        return this.nama;
+    }
+    
+}
