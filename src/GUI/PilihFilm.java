8fd4e4fc5739022fa14ca95d979cdd6392283944
Yunia <Pbo@172.16.100.134>
2019-05-29 07:59:25 +0700

    Membuat GUI pilih Kursi+menampilkan pilih kursi

--- src/GUI/PilihFilm.java
+++ src/GUI/PilihFilm.java
@@ -87,6 +87,11 @@ public class PilihFilm extends javax.swing.JFrame {
         jScrollPane2.setViewportView(jTextArea1);
 
         jButton1.setText("Pesan Kursi");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -157,6 +162,14 @@ public class PilihFilm extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        PilihKursi PK = new PilihKursi();
+        PilihFilm PL = new PilihFilm();
+        PK.setVisible(true);
+        PL.setVisible(false);
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
