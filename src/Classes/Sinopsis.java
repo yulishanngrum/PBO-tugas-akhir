ab88475ce262aeb8cb244feee0225911dea32464
Zanuba <niken@niken-PC>
2019-06-11 20:56:47 +0700

    Menambahkan kelas sinopsis berisi array sinopsis film pengganti database

---
+++ src/Classes/Sinopsis.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+/**
+ *
+ * @author niken
+ */
+public class Sinopsis {
+    private String[] sinopsisfilm = {"Dengan kekuatan infinity Stone,"
+            + " \nThanos menciptakan fenomena The Decimation dimana "
+            + "\nseparuh makhluk musnah, Akankah avenger dpt "
+            + "\nmengatasinya","Memiliki anak adalah harapan semua ortu, "
+            + "\nTidak memiliki anak dan dalam  \nsebuah kejadian aneh sepasang "
+            + "\nsuami istri menemukann anak yg misterius","Seorang pemuda biasa mendapatkan, "
+            + "\nkekuasaan melalui jin yang ditemukannya  "
+            + "\ndi dalam sebuah lampu ajaib. ","Dunia Terancam karena ulah Steppenwolfs, "
+            + "\nperebutan Mother Boxes di bumi  \noleh Justice league dan villain pun terjadi "}; 
+
+    public String getSinopsisfilm() {
+        return sinopsisfilm[0];
+    }
+        public String getSinopsisfilm1() {
+        return sinopsisfilm[1];
+    }
+        public String getSinopsisfilm2() {
+        return sinopsisfilm[2];
+    }
+        public String getSinopsisfilm3() {
+        return sinopsisfilm[3];
+    }
+   
+  
+}
--- src/GUI/SinopsisFilm.java
+++ src/GUI/SinopsisFilm.java
@@ -6,6 +6,7 @@
 package GUI;
 
 
+
 /**
  *
  * @author Yunia
