1cee2c6fbbf3f93703650d1ef2fc0e97f9997e71
Alwin <niken@niken-PC>
2019-06-12 17:37:28 +0700

    Menambahkan Kelas Harga untuk menyimpan harga

---
+++ src/Classes/Harga.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+/**
+ *
+ * @author niken
+ */
+public class Harga {
+    protected String[] hargafilm = {"30000","35000","32000","34000"};
+
+    public String getHargafilm() {
+        return hargafilm[0];
+    }
+    public String getHargafilm1() {
+        return hargafilm[1];
+    }    
+    public String getHargafilm2() {
+        return hargafilm[2];
+    }    
+    public String getHargafilm3() {
+        return hargafilm[3];
+    }    
+}
